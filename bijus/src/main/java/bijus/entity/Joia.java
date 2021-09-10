9e9a06df99df3f69c446ce5e775a55fe06b0826c
Luiz Sodre <tqi_lsodre@uolinc.com>
2021-09-10 17:45:31 -0300

    ajustes na biju 10-09-2021

---
+++ bijus/src/main/java/bijus/entity/Joia.java
@@ -0,0 +1,13 @@
+package bijus.entity;
+
+public class Joia extends Peca {
+
+	private static final String CATEGORIA_JOIA = "J\u00F3ia";
+
+	
+	@Override
+	public String getCategoria() {
+		return CATEGORIA_JOIA;
+	}
+	
+}
---
+++ bijus/src/main/java/bijus/entity/Peca.java
@@ -0,0 +1,43 @@
+package bijus.entity;
+
+public abstract class Peca {
+
+	private String descricao;
+	private String categoria;
+	private String imagem;
+	private String status;
+	private Double preco;
+
+	
+	public String getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	public String getImagem() {
+		return imagem;
+	}
+	public void setImagem(String imagem) {
+		this.imagem = imagem;
+	}
+	public Double getPreco() {
+		return preco;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+}

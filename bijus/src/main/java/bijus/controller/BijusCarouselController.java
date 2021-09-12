39beef31bdb2c49c614626c883cd51e48d3c67c5
Luiz Sodre <luizsodrerj@gmail.com>
2021-09-11 23:43:39 -0300

    ajustes em 11-09-2021

--- bijus/src/main/java/bijus/controller/BijusCarouselController.java
+++ bijus/src/main/java/bijus/controller/BijusCarouselController.java
@@ -3,12 +3,14 @@ package bijus.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 import bijus.entity.Bijuteria;
 import bijus.entity.Joia;
 import bijus.service.BijusService;
 
+@ManagedBean
 @ViewScoped
 public class BijusCarouselController {
 
@@ -23,9 +25,6 @@ public class BijusCarouselController {
 		joias.addAll(
 			BijusService.getJoias()	
 		);
-	}
-	
-	public void setFiles() {
 	}
 
 	public List<Bijuteria> getBijus() {
